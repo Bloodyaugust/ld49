7f17accc99d8187f6a1d4787b3ba87c82ca41c1c
Bloodyaugust <greysonrichey@gmail.com>
2021-10-03 16:45:10 -0600

    Add thirst behavior, mechanics, and indicator for creatures

--- actors/creatures/Creature.gd
+++ actors/creatures/Creature.gd
@@ -1,6 +1,7 @@
 extends Sprite
 
 const consume_range:float = 5.0
+const drink_range:float = 100.0
 const state_indicator_textures:Dictionary = {
   "thirsty": preload("res://sprites/THIRSTY.png"),
   "hunting": preload("res://sprites/hunting.png"),
@@ -9,7 +10,9 @@ const state_indicator_textures:Dictionary = {
 enum creature_states {
   IDLE,
   CONSUMING,
+  DRINKING,
   HUNTING,
+  THIRSTY,
   WANDERING
 }
 
@@ -17,6 +20,7 @@ export var consumes:String
 export var consume_amount:int
 export var consume_meter:float
 export var speed:float
+export var thirst_meter:float = 5.0
 export var type:String
 export var wander_time_max:float = 2.5
 export var wander_time_min:float = 0.5
@@ -28,6 +32,7 @@ var spawner:Node2D
 
 onready var _animation_player:AnimationPlayer = find_node("AnimationPlayer")
 onready var _mouth:Node2D = find_node("Mouth")
+onready var _pond:Node2D = get_tree().get_nodes_in_group("pond")[0]
 onready var _state_indicator:TextureRect = find_node("Indicator")
 
 var _current_consume_meter:float
@@ -35,6 +40,7 @@ var _resource_target:Node2D
 var _state:int
 var _time_to_idle:float
 var _time_to_wander:float
+var _thirst:float
 var _wander_target:Vector2
 
 func consume(_amount:int) -> void:
@@ -68,12 +74,19 @@ func _find_resource_target() -> void:
     _resource_target = _eligible_resources[randi() % _eligible_resources.size()]
 
 func _on_consume_complete() -> void:
-  _resource_target.consume(consume_amount)
-  _current_consume_meter = 0
+  match _state:
+    creature_states.CONSUMING:
+      _resource_target.consume(consume_amount)
+      _current_consume_meter = 0
+    creature_states.DRINKING:
+      _thirst = thirst_meter
+
   _state = creature_states.IDLE
 
 func _process(delta):
   match _state:
+    creature_states.DRINKING, creature_states.THIRSTY:
+      _state_indicator.texture = state_indicator_textures.thirsty
     creature_states.HUNTING:
       _state_indicator.texture = state_indicator_textures.hunting
     _:
@@ -82,9 +95,10 @@ func _process(delta):
   if dead:
     _state_indicator.texture = null
 
-  if !dead && _state != creature_states.CONSUMING:
+  if !dead && _state != creature_states.CONSUMING && _state != creature_states.DRINKING:
     update()
     _current_consume_meter += delta
+    _thirst -= delta
 
     if _time_to_idle <= 0 && _state == creature_states.WANDERING:
       _animation_player.play("idle")
@@ -96,11 +110,14 @@ func _process(delta):
       _time_to_idle = rand_range(wander_time_min, wander_time_max)
       _wander_target = spawner.global_position + (Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized() * rand_range(0, spawner.spawn_move_range))
 
-    if _current_consume_meter >= consume_meter && _state != creature_states.HUNTING:
+    if _current_consume_meter >= consume_meter && _state != creature_states.HUNTING && _state != creature_states.THIRSTY:
         _find_resource_target()
 
         if GDUtil.reference_safe(_resource_target):
           _state = creature_states.HUNTING
+
+    if _thirst <= 0 && _state != creature_states.THIRSTY:
+      _state = creature_states.THIRSTY
 
     var _move_amount = speed * delta
     match _state:
@@ -119,6 +136,14 @@ func _process(delta):
             _state = creature_states.CONSUMING
         else:
           _state = creature_states.IDLE
+      creature_states.THIRSTY:
+        global_position += global_position.direction_to(_pond.global_position) * _move_amount
+
+        if global_position.distance_to(_pond.global_position) <= drink_range:
+          print("Drinking at distance: " + str(global_position.distance_to(_pond.global_position)))
+          _animation_player.play("consume")
+          _state = creature_states.DRINKING
+        
       creature_states.WANDERING:
         if global_position.distance_to(_wander_target) <= _move_amount:
           _wander_target = spawner.global_position + (Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized() * rand_range(0, spawner.spawn_move_range))
@@ -132,6 +157,7 @@ func _process(delta):
 func _ready():
   _current_consume_meter = 0
   _state = creature_states.IDLE
+  _thirst = thirst_meter
   _wander_target = spawner.global_position
 
   var _new_creatures_state = Store.state.creatures.duplicate(true)
