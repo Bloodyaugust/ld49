dfe6fb91a3595a32f6466545aba40e48d7885daa
Bloodyaugust <greysonrichey@gmail.com>
2021-10-03 15:57:25 -0600

    Add splash screen

--- autoloads/Store.gd
+++ autoloads/Store.gd
@@ -37,7 +37,7 @@ func set_state(state_key: String, new_state) -> void:
   print("State changed: ", state_key, " -> ", state[state_key])
   
 func _initialize():
-  set_state("client_view", ClientConstants.CLIENT_VIEW_MAIN_MENU)
+  set_state("client_view", ClientConstants.CLIENT_VIEW_SPLASH)
   set_state("game", GameConstants.GAME_OVER)
 
 func _ready():
